4a5c94ea20bdc6b09a6a74df4b0e68ff7dbacc78
Ricardo <rh845@live.mdx.ac.uk>
2020-04-29 22:53:32 +0100

    V0.1
    Gravity and manipulation mechanics

---
+++ Assets/Scripts/DieOnCollision.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DieOnCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "GravityObject")
+        {
+            if (collision.relativeVelocity.magnitude >= 5)
+            {
+                //Player dies
+                Debug.Log(collision.relativeVelocity.magnitude);
+                Debug.Log("player died");
+            }
+            else
+            {
+                Debug.Log(collision.relativeVelocity.magnitude);
+                Debug.Log("player did not die");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GravityControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Physics.gravity = new Vector3(0, -10f, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Physics.gravity = new Vector3(0, 10f, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Physics.gravity = new Vector3(10f, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            Physics.gravity = new Vector3(-10f, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            Physics.gravity = new Vector3(0, 0, 10f);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            Physics.gravity = new Vector3(0, 0, -10f);
+        }
+    }
+}
